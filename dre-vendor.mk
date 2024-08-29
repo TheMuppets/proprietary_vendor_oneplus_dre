76433884f6c65f1e3b727ed80fc4e5fc0de8ed66
Albert Tang <tangalbert919@yahoo.com>
2024-08-29 18:11:53 -0500

    dre: Return to stock IMS system stack
    Change-Id: Ib404419ac13362c4cf598023bfb13e3b233cf5b8

--- dre-vendor.mk
+++ dre-vendor.mk
@@ -1767,9 +1767,13 @@ PRODUCT_PACKAGES += \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
+    oplus-ims-ext \
     qcrilhook \
     vendor.qti.hardware.alarm-V1.0-java \
+    vendor.qti.ims.connection-V1.0-java \
     vendor.qti.ims.factory-V2.0-java \
+    vendor.qti.ims.rcssip-V1.0-java \
+    vendor.qti.ims.rcsuce-V1.0-java \
     android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti \
     c2_manifest_vendor \
