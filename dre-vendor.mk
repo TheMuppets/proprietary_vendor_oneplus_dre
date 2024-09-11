cd4369744006f084ed483306ad0d6f42ce0b94a8
Albert Tang <tangalbert919@yahoo.com>
2024-09-11 08:49:31 -0500

    dre: Add needed XML fixes
    The following changes were applied:
    * Regenerate without XML fixup
    * Add xml suffix to prebuilt_etc_xml modules
    
    Change-Id: Ie40bb20a951040f1d0f49da63b9c5caf1006ca74

--- dre-vendor.mk
+++ dre-vendor.mk
@@ -1774,13 +1774,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.factory-V2.0-java \
     vendor.qti.ims.rcssip-V1.0-java \
     vendor.qti.ims.rcsuce-V1.0-java \
-    android.hardware.gnss@2.1-service-qti \
-    android.hardware.neuralnetworks@1.3-service-qti \
-    c2_manifest_vendor \
-    vendor.qti.gnss@4.0-service \
-    vendor.qti.hardware.servicetracker@1.2-service \
-    manifest_android.hardware.drm@1.3-service.widevine \
-    manifest_oplus_fingerprint
+    android.hardware.gnss@2.1-service-qti.xml \
+    android.hardware.neuralnetworks@1.3-service-qti.xml \
+    c2_manifest_vendor.xml \
+    vendor.qti.gnss@4.0-service.xml \
+    vendor.qti.hardware.servicetracker@1.2-service.xml \
+    manifest_android.hardware.drm@1.3-service.widevine.xml \
+    manifest_oplus_fingerprint.xml
 
 PRODUCT_PACKAGES += \
     libimscamera_jni_libimscamera_jni_symlink64 \
