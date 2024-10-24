67fe7dd25750529256407288da56803cdd1c9e5b
LuK1337 <priv.luk@gmail.com>
2024-10-24 21:46:02 +0200

    dre: Declare adsp_avs_config.acdb symlink during extraction
    Change-Id: I5a328db61cbd83fe292522b90f21b85e94986381

--- dre-vendor.mk
+++ dre-vendor.mk
@@ -1676,6 +1676,7 @@ PRODUCT_PACKAGES += \
     system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
     system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
     system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
+    odm_etc_acdbdata_adsp_avs_config_acdb \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
     vendor_lib_libq3dtools_adreno_so \
