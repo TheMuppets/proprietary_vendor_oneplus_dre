5cccf19bfb34fbdb4ff90e6553c621edcb5ee3ce
Bruno Martins <bgcngm@gmail.com>
2026-05-30 12:55:05 +0100

    dre: De-blob oplus interfaces
    Change-Id: I6c45d39c2cc36c8285152e6472323017a08355c5

--- dre-vendor.mk
+++ dre-vendor.mk
@@ -1300,7 +1300,6 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.5 \
     vendor.display.postproc@1.0 \
     vendor.oplus.hardware.cammidasservice@1.0 \
-    vendor.oplus.hardware.radio-V2-ndk_platform \
     vendor.qti.data.factory@2.0 \
     vendor.qti.data.factory@2.1 \
     vendor.qti.data.factory@2.2 \
@@ -1551,7 +1550,6 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.commondcs@1.0 \
     vendor.oplus.hardware.ims@1.0 \
     vendor.oplus.hardware.orms@1.0 \
-    vendor.oplus.hardware.performance-V1-ndk_platform \
     vendor.oplus.hardware.radio@1.0 \
     odm_lib_rfsa_adsp_capi_v2_aptX_CLHDAD_Encoder_so \
     odm_lib_rfsa_adsp_libVDHexagonBlurless_v2_skel_so \
